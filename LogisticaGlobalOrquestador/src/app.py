77e30a450b99bbf4a77623eed0d3f5215d63ef2b
AleRuizdV <al.ruizdevinaspre@duocuc.cl>
2025-05-07 22:32:46 -0400

    Gateaway from the favicon.ico (now really)

--- LogisticaGlobalOrquestador/src/app.py
+++ LogisticaGlobalOrquestador/src/app.py
@@ -7,6 +7,13 @@ load_dotenv()
 
 app = Flask(__name__)
 
+# Temporal solution =========================
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'),
+                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
+# ===========================================
+# Rutas de la API ===========================
 @app.route('/orquestar', methods=['POST'])
 def route_orquestar_servicio():
     return orquestar_servicio()
@@ -18,6 +25,7 @@ def route_obtener_informacion_servicio(id):
 @app.route('/saludo', methods=['GET'])
 def saludo():
     return jsonify({"mensaje": "¡Hola desde la API de Logística Global!"}), 200
+# ===========================================
 
 if __name__ == '__main__':
     app.run(debug=True, port=int(os.getenv("PORT", 5000)))
